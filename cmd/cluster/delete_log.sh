edb57f5778200c38e996268009915ad283564855
Ubuntu <ubuntu@ip-172-31-3-44.us-west-1.compute.internal>
2020-04-04 02:05:31 +0000

    Modifications on .sh files

---
+++ cmd/cluster/delete_log.sh
@@ -0,0 +1,3 @@
+cmd="rm S0.log S1.log S2.log S3.log master.log"
+echo $cmd
+eval $cmd
--- cmd/cluster/run_cluster.sh
+++ cmd/cluster/run_cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-configPath=$1
+configPath='/home/ubuntu/go/src/github.com/QuarkChain/goquarkchain/tests/cluster_config_local.json'
+
+# /home/ubuntu/go/src/github.com/QuarkChain/goquarkchain/tests/cluster_config_local.json
 
 slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
 slaveInfoArr=(${slaveInfo})
@@ -17,7 +19,7 @@ localIPInfo=`ifconfig | grep inet | grep -v inet6 | grep -v 127 | cut -d ' ' -f1
 for index in ${!slaveInfoArr[@]}; do
 # echo $index
 if [ ${slaveIPInfoArr[index]} == $localIPInfo ]; then
-	 cmd="./cluster --cluster_config "${configPath}" --service "${slaveInfoArr[index]}">> "${slaveInfoArr[index]}".log 2>&1 &"
+	 cmd="./cluster --cluster_config "${configPath}" --verbosity 4 --service "${slaveInfoArr[index]}">> "${slaveInfoArr[index]}".log 2>&1 &"
 	 echo $cmd
 	 eval $cmd
 fi
@@ -27,6 +29,11 @@ done
 sleep 10s
 
 # start master
-cmd="./cluster --cluster_config "${configPath}" >>master.log 2>&1 &"
+cmd="./cluster --cluster_config "${configPath}" --verbosity 4 >>master.log 2>&1 &"
 echo $cmd
 eval $cmd
+
+# start mining
+
+# curl -X POST -H 'content-type: application/json' --data '{"jsonrpc":"2.0","method":"setMining","params":[true],"id":0}' http://127.0.0.1:38491
+
