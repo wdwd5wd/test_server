a15e8ff5b6cf92292a30135369e7e7823dfb03c7
Ubuntu <ubuntu@ip-172-31-3-44.us-west-1.compute.internal>
2020-04-04 02:12:07 +0000

    Add funcs

--- cluster/shard/shard_migration.go
+++ cluster/shard/shard_migration.go
@@ -10,13 +10,60 @@ import (
 // Retrieve & remove all queued & pending transcations originated from given account on
 // that shard. Mining will restart. Returned transcations are sorted by nonce.
 func (s *ShardBackend) PopAccountTranscations(account common.Address) types.Transactions {
-	txs := s.MinorBlockChain.PopAccountTranscations(account)
+	// Restart mining
+	// s.minerSem.Release(1)
+	//s.mu.Lock()
+
+	// if s.miner.IsMining() {
+	// 	log.Debug("Terminate mining")
+	// 	s.miner.SetMining(false)
+	// }
+	//s.mu.Unlock()
+
+	txs := s.MinorBlockChain.PopAccountTxs(account)
 	log.Debug("Transcations poped from pool", "size", len(txs))
 
-	// // Restart mining
-	// if s.miner.IsMining() {
+	//s.mu.Lock()
+	// if !s.miner.IsMining() {
 	// 	log.Debug("Restart mining")
+	// 	s.miner.SetMining(true)
+	// }
+	//s.mu.Unlock()
+
+	return txs
+}
+
+func (s *ShardBackend) RestartMining(ok bool) {
+
+	s.mu.Lock()
+
+	if s.miner.IsMining() && !ok {
+		log.Debug("Terminate mining")
+		s.miner.SetMining(false)
+	}
+
+	if !s.miner.IsMining() && ok {
+		log.Debug("Restart mining")
+		s.miner.SetMining(true)
+	}
+
+	s.mu.Unlock()
+}
+
+// Retrieve all queued & pending transcations originated from given account on
+// that shard. Mining will restart. Returned transcations are sorted by nonce.
+func (s *ShardBackend) CountAccountTranscations(account common.Address) types.Transactions {
+	// Restart mining
+	// if s.miner.IsMining() {
+	// 	log.Debug("Terminate mining")
 	// 	s.miner.SetMining(false)
+	// }
+
+	txs := s.MinorBlockChain.CountBroadcastTxs(account)
+	log.Debug("Transcations COUNTED from pool", "size", len(txs))
+
+	// if !s.miner.IsMining() {
+	// 	log.Debug("Restart mining")
 	// 	s.miner.SetMining(true)
 	// }
 
